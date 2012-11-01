42e3e6291372cea5e3311e7ca9127d504c5a5ef3
shtaxxx <shta.ky1018@gmail.com>
2012-11-01 18:41:07 +0900

    shoter code

--- jitter-clock.v
+++ jitter-clock.v
@@ -2,25 +2,31 @@
 module test;
   reg CLK;
   reg [31:0] rand_val;
+  integer clock_half_period;
+  
   always @(posedge CLK) begin
     rand_val <= {$random};
   end
+  
+  initial begin
+    clock_half_period = 100;
+  end
+  
+  always @* begin
+    clock_half_period = 100;
+    if(rand_val[3:0] > 8) clock_half_period = 200;
+  end
+  
   initial begin
     CLK = 0;
-    while(1) begin
-      if(rand_val[3:0] > 8) begin
-        #100;
-        CLK = !CLK;
-      end else begin
-        #200;
-        CLK = !CLK;
-      end
-    end
+    forever #(clock_half_period) CLK = !CLK;
   end
+  
   initial begin
     $dumpfile("uut.vcd");
     $dumpvars(0, CLK, rand_val);
   end
+  
   initial begin
     #10000;
     $finish;
